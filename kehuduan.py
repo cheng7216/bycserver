1ab05c0a07728058d3464d61e93a303507b3cbbb
cheng7216 <bycbyc13963326302@163.com>
2024-10-21 21:26:10 +0800

    Add files via upload

---
+++ kehuduan.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import socket
+
+page = st.sidebar.radio("我的主页", ["主页"])
+
+# 创建一个socket对象
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+# 获取本地机器上的IP和端口
+host = '0.0.0.0'  # 使用0.0.0.0让服务器监听所有接口
+port = 26598
+ 
+# 绑定socket到端口
+server_socket.bind((host, port))
+ 
+# 设置最大连接数，等待连接的客户端数量
+server_socket.listen(1)
+ 
+print(f"Server is listening on {host}:{port}...")
+ 
+# 接受一个新的连接
+client_socket, addr = server_socket.accept()
+ 
+print(f"Connected to client {addr[0]}:{addr[1]}")
+ 
+# 接收数据
+received_data = client_socket.recv(1024)
+print("Received data:", received_data.decode())
+ 
+# 关闭socket连接
+client_socket.close()
+server_socket.close()
+
+def page1():
+    if st.button("跳转"):
+        st.write(received_data)
